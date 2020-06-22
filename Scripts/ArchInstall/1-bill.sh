f1f3ce94da242e9fbd7f43a48aea7bb774093c61
Szabó Máté <53392547+PadBoy@users.noreply.github.com>
2020-06-22 17:40:46 +0200

    Create 1-bill.sh

---
+++ Scripts/ArchInstall/1-bill.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+echo "########################################"
+echo "# - Billentyűzet beállítása magyarra - #"
+echo "########################################"
+echo "Egyes karakterek így sem lesznek elérhetők!"
