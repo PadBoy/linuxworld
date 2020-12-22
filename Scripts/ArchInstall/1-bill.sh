0e0741d93f77ea9a2e426b58dc180fd167cbe114
Szabó Máté <53392547+PadBoy@users.noreply.github.com>
2020-12-22 19:01:04 +0100

    Delete 1-bill.sh

--- Scripts/ArchInstall/1-bill.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/zsh
-echo
-echo '######################################'
-echo '# A billentyuzet atallitasa magyarra #'
-echo '######################################'
-echo
-loadkeys hu
-
-echo 'atallitva'
